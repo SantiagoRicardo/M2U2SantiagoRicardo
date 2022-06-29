3ff41db3c9e80154781edc40d291aa536b9788fe
Santiago Ricardo Ramirez <sricard2@ibero.edu.co>
2022-06-29 01:07:03 -0500

    Montando Logica Completa Del Ejercicio 1

--- Ejercicio1/logicaMaterias.js
+++ Ejercicio1/logicaMaterias.js
@@ -1,26 +1,44 @@
 let nombre   = document.getElementById('nombre');
 let materias = document.getElementById('materias');
-const boton  = document.getElementById('boton');
-const div    = document.getElementById('label'); 
-const div2   = document.getElementById('inputValorMaterias');
-const div3 = document.getElementById('botonEnviarValores');
-const div4 = document.getElementById('factura');
-
-
+const 
+    boton  = document.getElementById('boton'),
+    div    = document.getElementById('inputsGenerados'), 
+    div2   = document.getElementById('inputValorMaterias'),
+    div3   = document.getElementById('resultSuma')
+ 
 boton.addEventListener('click', () => {
+
     let nombre   = document.getElementById('nombre').value;
     let materias = document.getElementById('materias').value;
- 
-    div.innerHTML = `<label for="">Digite El Valor De Cada Materia</label> <br><br>`;
-    div2.innerHTML = `<input type="number" name="" id="numMaterias" placeholder="Digite Los Valores Aqui"> <br><br>`;
-    div3.innerHTML = `<input type="button" id="enviarValores" value="Enviar Valores">`;
-});
+    let suma = 0;
 
-div3.addEventListener('click', () => {
+    for (let i = 1; i <= materias; i++) {
+        div.innerHTML += `<input type="number" name="" id="${i}" placeholder="Valor Materia ${i}"> <br><br>`;
+    }
+    div2.innerHTML += `<input type="button" name="" id="boton2" value="¡Vamos!"> <br><br>`;
+
+    //Este Codigo Se Ejecutará Despues Que Se Presione El Boton "¡Vamos!"
     
-    let numMaterias = document.getElementById('numMaterias').value;
-    let valores = [];
-    
-    div4.innerHTML = `El valor De Las Materia Es De ${numMaterias}`;
+    div2.addEventListener('click', () => {
+
+        for (let i = 1; i <= materias; i++) {
+            const valor = document.getElementById(i).value;
+            suma += parseInt(valor);
+        }
+        const 
+            papeleria     = 20000,
+            carnet        = 8000,
+            porcentajeDto = 20,
+            descuento     = (porcentajeDto * suma) / 100,
+            restaValor    = (suma - descuento),
+            totalPago     = restaValor + papeleria + carnet
+
+        div3.innerHTML += `<p> Hola ${nombre}, Que Tal? </p>
+        <p>El Total A Pagar Por Las Materias Es De: ${suma}</p>
+        <p>El Descuento Aplicado Al Total De Las Materias Es Del 20%, Es Decir: ${descuento}</p>
+        <p>Los Costos Fijos Son: <br> Papeleria: $ ${papeleria} <br> Carnet: $ ${carnet}</p>
+        <p>Total A Pagar: ${totalPago}</p>
+        <p>NOTA: RECUERDE, EL VALOR TOTAL A PAGAR CONTIENE LOS DESCUENTOS APLICADOS ANTERIORMENTE</p>`;
+    });
 });
 
